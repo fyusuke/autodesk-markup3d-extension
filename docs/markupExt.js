b07d7a3f04e9d924b14c1782855129029d63830c
yusuke <yusukechief@gmail.com>
2023-03-04 00:52:09 +0900

    implement mark add function

--- docs/markupExt.js
+++ docs/markupExt.js
@@ -1,25 +1,27 @@
 // MarkupExt.js
 function markup3d(viewer, options) {
-    Autodesk.Viewing.Extension.call(this, viewer, options);
-    this.raycaster = new THREE.Raycaster();
-    this.raycaster.params.PointCloud.threshold = 5; // hit-test markup size.  Change this if markup 'hover' doesn't work
-    this.size = 150.0; // markup size.  Change this if markup size is too big or small
-    this.lineColor = 0xcccccc; // off-white
-    this.labelOffset = new THREE.Vector3(120,120,0);  // label offset 3D line offset position
-    this.xDivOffset = -0.2;  // x offset position of the div label wrt 3D line.
-    this.yDivOffset = 0.4;  // y offset position of the div label wrt 3D line.
+  Autodesk.Viewing.Extension.call(this, viewer, options);
+  this.raycaster = new THREE.Raycaster();
+  this.raycaster.params.PointCloud.threshold = 5; // hit-test markup size.  Change this if markup 'hover' doesn't work
+  this.size = 150.0; // markup size.  Change this if markup size is too big or small
+  this.lineColor = 0xcccccc; // off-white
+  this.labelOffset = new THREE.Vector3(120, 120, 0); // label offset 3D line offset position
+  this.xDivOffset = -0.2; // x offset position of the div label wrt 3D line.
+  this.yDivOffset = 0.4; // y offset position of the div label wrt 3D line.
 
-    this.scene = viewer.impl.scene; // change this to viewer.impl.sceneAfter with transparency, if you want the markup always on top.
-    this.markupItems = []; // array containing markup data
-    this.pointCloud; // three js point-cloud mesh object
-    this.line3d; // three js point-cloud mesh object
-    this.camera = viewer.impl.camera;
-    this.hovered; // index of selected pointCloud id, based on markupItems array
-    this.selected; // index of selected pointCloud id, based on markupItems array
-    this.label; // x,y div position of selected pointCloud. updated on mouse-move
-    this.offset; // global offset
+  this.scene = viewer.impl.scene; // change this to viewer.impl.sceneAfter with transparency, if you want the markup always on top.
+  this.markupItems = []; // array containing markup data
+  this.pointCloud; // three js point-cloud mesh object
+  this.line3d; // three js point-cloud mesh object
+  this.camera = viewer.impl.camera;
+  this.hovered; // index of selected pointCloud id, based on markupItems array
+  this.selected; // index of selected pointCloud id, based on markupItems array
+  this.label; // x,y div position of selected pointCloud. updated on mouse-move
+  this.offset; // global offset
+  this.isAddMode = false;
+  this.addButton;
 
-    this.vertexShader = `
+  this.vertexShader = `
         uniform float size;
         varying vec3 vColor;
         void main() {
@@ -28,9 +30,9 @@ function markup3d(viewer, options) {
             gl_PointSize = size * ( size / (length(mvPosition.xyz) + 1.0) );
             gl_Position = projectionMatrix * mvPosition;
         }
-    `
+    `;
 
-    this.fragmentShader = `
+  this.fragmentShader = `
         uniform sampler2D tex;
         varying vec3 vColor;
         void main() {
@@ -38,147 +40,257 @@ function markup3d(viewer, options) {
             gl_FragColor = gl_FragColor * texture2D(tex, vec2((gl_PointCoord.x+vColor.y*1.0)/4.0, 1.0-gl_PointCoord.y));
             if (gl_FragColor.w < 0.5) discard;
         }
-    `
-
+    `;
 }
 
 markup3d.prototype = Object.create(Autodesk.Viewing.Extension.prototype);
 markup3d.prototype.constructor = markup3d;
 
-markup3d.prototype.updateHitTest = function(event) {
-    // on mouse move event, check if ray hit with pointcloud, move selection cursor
-    // https://stackoverflow.com/questions/28209645/raycasting-involving-individual-points-in-a-three-js-pointcloud
-    if (!this.pointCloud) return;
+markup3d.prototype.updateHitTest = function (event) {
+  // on mouse move event, check if ray hit with pointcloud, move selection cursor
+  // https://stackoverflow.com/questions/28209645/raycasting-involving-individual-points-in-a-three-js-pointcloud
+  if (!this.pointCloud) return;
 
-    let canvas = event.target;
-    let _x = event.offsetX * canvas.width / canvas.clientWidth | 0;
-    let _y = event.offsetY * canvas.height / canvas.clientHeight | 0;
-    let x = 2 * (_x / canvas.clientWidth) - 1; // scales from -1 to 1
-    let y = -2 * (_y / canvas.clientHeight) + 1; // scales from -1 to 1, with direction reversed
+  let canvas = event.target;
+  let _x = ((event.offsetX * canvas.width) / canvas.clientWidth) | 0;
+  let _y = ((event.offsetY * canvas.height) / canvas.clientHeight) | 0;
+  let x = 2 * (_x / canvas.clientWidth) - 1; // scales from -1 to 1
+  let y = -2 * (_y / canvas.clientHeight) + 1; // scales from -1 to 1, with direction reversed
 
-    var vector = new THREE.Vector3(x, y, 0.5).unproject(this.camera);
-    this.raycaster.set(this.camera.position, vector.sub(this.camera.position).normalize());
-    var nodes = this.raycaster.intersectObject(this.pointCloud);
-    if (nodes.length > 0) {
-      if (this.hovered || this.hovered === 0)
-            this.geometry.colors[this.hovered].r = 1.0;
-        this.hovered = nodes[0].index;
-        this.geometry.colors[this.hovered].r = 2.0;
-        this.geometry.colorsNeedUpdate = true;
-        window.viewer.impl.invalidate(true);
-    }
-}
-
-markup3d.prototype.unload = function() {
-    return true;
+  var vector = new THREE.Vector3(x, y, 0.5).unproject(this.camera);
+  this.raycaster.set(
+    this.camera.position,
+    vector.sub(this.camera.position).normalize()
+  );
+  var nodes = this.raycaster.intersectObject(this.pointCloud);
+  if (nodes.length > 0) {
+    if (this.hovered || this.hovered === 0)
+      this.geometry.colors[this.hovered].r = 1.0;
+    this.hovered = nodes[0].index;
+    this.geometry.colors[this.hovered].r = 2.0;
+    this.geometry.colorsNeedUpdate = true;
+    window.viewer.impl.invalidate(true);
+  }
 };
 
-markup3d.prototype.load = function() {
-    var self = this;
-    this.material = null;
-    this.offset = window.viewer.model.getData().globalOffset; // use global offset to align pointCloud with lmv scene
-
-    // setup listeners for new data and mouse events
-    window.addEventListener("newData", e => { this.setMarkupData( e.detail ) }, false);
-    document.addEventListener('mousedown', e => { this.onClick(e) }, true);
-    document.addEventListener('touchstart', e => { this.onClick(e.changedTouches[0]) }, false);
-    document.addEventListener('mousemove', e => { this.onMouseMove(e) }, false);
-    document.addEventListener('touchmove', e => { this.onMouseMove(e.changedTouches[0]) }, false);
-    document.addEventListener('mousewheel', e => { this.onMouseMove(e) }, true);
-
-
-    // Load markup points into Point Cloud
-    this.setMarkupData = function(data) {
-        this.markupItems = data;
-        this.geometry = new THREE.Geometry();
-        data.map(item => {
-            point = (new THREE.Vector3(item.x, item.y, item.z));
-            this.geometry.vertices.push(point);
-            this.geometry.colors.push(new THREE.Color(1.0, item.icon, 0)); // icon = 0..2 position in the horizontal icons.png sprite sheet
-        });
-        this.initMesh_PointCloud();
-        this.initMesh_Line();
-    };
-
-
-    this.initMesh_PointCloud = function() {
-        if (this.pointCloud) 
-            this.scene.remove(this.pointCloud); //replace existing pointCloud Mesh
-        else {
-            // create new point cloud material
-            var texture = THREE.ImageUtils.loadTexture("img/icons.png");
-            this.material = new THREE.ShaderMaterial({
-                vertexColors: THREE.VertexColors,
-                fragmentShader: this.fragmentShader,
-                vertexShader: this.vertexShader,
-                depthWrite: true,
-                depthTest: true,
-                uniforms: {
-                    size: { type: "f", value: this.size },
-                    tex: { type: "t", value: texture }
-                }
-            });
-        }
-        this.pointCloud = new THREE.PointCloud(this.geometry, this.material);
-        this.pointCloud.position.sub( this.offset );
-        if (!this.viewer.overlays.hasScene('custom-scene')) {
-          this.viewer.overlays.addScene('custom-scene');
-        }
-        this.viewer.overlays.addMesh(this.pointCloud , 'custom-scene');
-    }
-
-
-    this.initMesh_Line = function() {
-        var geom = new THREE.Geometry();
-        geom.vertices = [new THREE.Vector3(0, 0, 0),  new THREE.Vector3(0,1,1), new THREE.Vector3(1,1,1) ];
-        geom.faces = [new THREE.Face3(0,1,2)];
-        this.line3d = new THREE.Mesh( geom, new THREE.MeshBasicMaterial({ color: this.lineColor, side: THREE.DoubleSide }) );
-        this.line3d.position.sub( this.offset );
-        // this.scene.add(this.line3d);
-        if (!this.viewer.overlays.hasScene('custom-scene')) {
-          this.viewer.overlays.addScene('custom-scene');
-        }
-        this.viewer.overlays.addMesh(this.line3d, 'custom-scene');
-    }
-
-    this.update_Line = function() {
-        var position = this.pointCloud.geometry.vertices[this.selected].clone();
-        this.line3d.geometry.vertices[0] = position;
-        this.line3d.geometry.vertices[1].set( position.x + this.labelOffset.x * Math.sign(position.x), position.y + this.labelOffset.y, position.z + this.labelOffset.z );
-        this.line3d.geometry.vertices[2].set( position.x + this.labelOffset.x * Math.sign(position.x), position.y + 20 + this.labelOffset.y, position.z + this.labelOffset.z );
-        this.line3d.geometry.verticesNeedUpdate = true;
-    }
-
-    this.update_DivLabel = function(eventName){
-        var position = this.line3d.geometry.vertices[1].clone().sub(this.offset);
-        this.label = position.project(this.camera);
-        window.dispatchEvent(new CustomEvent(eventName, {
-            'detail': {
-                id: this.selected,
-                x: this.label.x + this.xDivOffset,
-                y: this.label.y + this.yDivOffset,
-            }
-        }));
-    }
-
-    // Dispatch Message when a point is clicked
-    this.onMouseMove = function(event) {
-        this.update_DivLabel('onMarkupMove');
-        this.updateHitTest(event);
-    }
-
-    this.onClick = function() {
-        this.updateHitTest(event);
-        if (!this.hovered && this.hovered !== 0) return;
-        this.selected = this.hovered;
-        this.update_Line();
-        this.update_DivLabel('onMarkupClick');
-        window.viewer.impl.invalidate(true);
-        window.viewer.clearSelection();
-    }
-
-    return true;
+markup3d.prototype.unload = function () {
+  return true;
 };
 
+markup3d.prototype.load = function () {
+  var self = this;
+  this.material = null;
+  this.offset = window.viewer.model.getData().globalOffset; // use global offset to align pointCloud with lmv scene
 
-Autodesk.Viewing.theExtensionManager.registerExtension('markup3d', markup3d);
+  // setup listeners for new data and mouse events
+  window.addEventListener(
+    "newData",
+    (e) => {
+      this.setMarkupData(e.detail);
+    },
+    false
+  );
+  document.addEventListener(
+    "mousedown",
+    (e) => {
+      this.onClick(e);
+    },
+    true
+  );
+  document.addEventListener(
+    "touchstart",
+    (e) => {
+      this.onClick(e.changedTouches[0]);
+    },
+    false
+  );
+  document.addEventListener(
+    "mousemove",
+    (e) => {
+      this.onMouseMove(e);
+    },
+    false
+  );
+  document.addEventListener(
+    "touchmove",
+    (e) => {
+      this.onMouseMove(e.changedTouches[0]);
+    },
+    false
+  );
+  document.addEventListener(
+    "mousewheel",
+    (e) => {
+      this.onMouseMove(e);
+    },
+    true
+  );
+
+  // Load markup points into Point Cloud
+  this.setMarkupData = function (data) {
+    this.markupItems = data;
+    this.geometry = new THREE.Geometry();
+    data.map((item) => {
+      point = new THREE.Vector3(item.x, item.y, item.z);
+      this.geometry.vertices.push(point);
+      this.geometry.colors.push(new THREE.Color(1.0, item.icon, 0)); // icon = 0..2 position in the horizontal icons.png sprite sheet
+    });
+    this.initMesh_PointCloud();
+    this.initMesh_Line();
+  };
+
+  this.initMesh_PointCloud = function () {
+    if (this.pointCloud)
+      this.scene.remove(this.pointCloud); //replace existing pointCloud Mesh
+    else {
+      // create new point cloud material
+      var texture = THREE.ImageUtils.loadTexture("img/icons.png");
+      this.material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        fragmentShader: this.fragmentShader,
+        vertexShader: this.vertexShader,
+        depthWrite: true,
+        depthTest: true,
+        uniforms: {
+          size: { type: "f", value: this.size },
+          tex: { type: "t", value: texture },
+        },
+      });
+    }
+    this.pointCloud = new THREE.PointCloud(this.geometry, this.material);
+    this.pointCloud.position.sub(this.offset);
+    if (!this.viewer.overlays.hasScene("custom-scene")) {
+      this.viewer.overlays.addScene("custom-scene");
+    }
+    this.viewer.overlays.addMesh(this.pointCloud, "custom-scene");
+  };
+
+  this.initMesh_Line = function () {
+    var geom = new THREE.Geometry();
+    geom.vertices = [
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(0, 1, 1),
+      new THREE.Vector3(1, 1, 1),
+    ];
+    geom.faces = [new THREE.Face3(0, 1, 2)];
+    this.line3d = new THREE.Mesh(
+      geom,
+      new THREE.MeshBasicMaterial({
+        color: this.lineColor,
+        side: THREE.DoubleSide,
+      })
+    );
+    this.line3d.position.sub(this.offset);
+    // this.scene.add(this.line3d);
+    if (!this.viewer.overlays.hasScene("custom-scene")) {
+      this.viewer.overlays.addScene("custom-scene");
+    }
+    this.viewer.overlays.addMesh(this.line3d, "custom-scene");
+  };
+
+  this.update_Line = function () {
+    var position = this.pointCloud.geometry.vertices[this.selected].clone();
+    this.line3d.geometry.vertices[0] = position;
+    this.line3d.geometry.vertices[1].set(
+      position.x + this.labelOffset.x * Math.sign(position.x),
+      position.y + this.labelOffset.y,
+      position.z + this.labelOffset.z
+    );
+    this.line3d.geometry.vertices[2].set(
+      position.x + this.labelOffset.x * Math.sign(position.x),
+      position.y + 20 + this.labelOffset.y,
+      position.z + this.labelOffset.z
+    );
+    this.line3d.geometry.verticesNeedUpdate = true;
+  };
+
+  this.update_DivLabel = function (eventName) {
+    var position = this.line3d.geometry.vertices[1].clone().sub(this.offset);
+    this.label = position.project(this.camera);
+    window.dispatchEvent(
+      new CustomEvent(eventName, {
+        detail: {
+          id: this.selected,
+          x: this.label.x + this.xDivOffset,
+          y: this.label.y + this.yDivOffset,
+        },
+      })
+    );
+  };
+
+  // Dispatch Message when a point is clicked
+  this.onMouseMove = function (event) {
+    this.update_DivLabel("onMarkupMove");
+    this.updateHitTest(event);
+  };
+
+  // this.onClick = function() {
+  //     this.updateHitTest(event);
+  //     if (!this.hovered && this.hovered !== 0) return;
+  //     this.selected = this.hovered;
+  //     this.update_Line();
+  //     this.update_DivLabel('onMarkupClick');
+  //     window.viewer.impl.invalidate(true);
+  //     window.viewer.clearSelection();
+  // }
+
+  this.onClick = function (e) {
+    // ピンマーク追加
+    if (this.isAddMode) {
+      console.log(e);
+      // オブジェクト以外をクリックしたときはnullが返る
+      const posInWorld = this.viewer.clientToWorld(e.offsetX, e.offsetY, true);
+      console.log("posInWorld", posInWorld);
+
+      if (posInWorld) {
+        const mark = {
+          icon: 0, // 0固定
+          x: posInWorld.point.x,
+          y: posInWorld.point.y,
+          z: posInWorld.point.z,
+        };
+        console.log("mark", mark);
+        this.setMarkupData([...this.markupItems, mark]);
+      }
+
+      this.isAddMode = false;
+      this.addButton.setState(1);
+    } else {
+      this.updateHitTest(event);
+      if (!this.hovered && this.hovered !== 0) {
+        // ピンマークがクリックされていないとき
+        // this.isAddMode = false;
+        // this.addButton.setState(1);
+        return;
+      }
+      this.selected = this.hovered;
+      console.log("this.selected", this.selected);
+      this.viewer.impl.invalidate(true);
+      this.viewer.clearSelection(); // not working
+    }
+  };
+  return true;
+};
+
+markup3d.prototype.onToolbarCreated = function (toolbar) {
+  // ピンマーク追加ボタン
+  this.addButton = new Autodesk.Viewing.UI.Button("AddPinMarkExtension");
+  this.addButton.onClick = (e) => {
+    this.isAddMode = !this.isAddMode;
+    this.addButton.setState(this.isAddMode ? 0 : 1); // 0はAddMode, 1は非AddMode
+  };
+  this.addButton.setToolTip("ピンマークを追加");
+  this.addButton.container.children[0].classList.add(
+    "fas",
+    "fa-map-marker-alt"
+  );
+
+  // サブツールバー
+  this.subToolbar =
+    this.viewer.toolbar.getControl("CustomToolbar") ||
+    new Autodesk.Viewing.UI.ControlGroup("CustomToolbar");
+  this.subToolbar.addControl(this.addButton);
+  toolbar.addControl(this.subToolbar);
+};
+
+Autodesk.Viewing.theExtensionManager.registerExtension("markup3d", markup3d);
